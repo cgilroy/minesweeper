a021d8dfa3770efdaf507b9520bb637bf9bda4b6
CJ Gilroy <c.gilroy9@gmail.com>
2019-11-30 08:12:49 -0700

    Add #place_bombs method

--- Board.rb
+++ Board.rb
@@ -1,7 +1,28 @@
 require_relative('Tile.rb')
+require('byebug')
 
 class Board
     def initialize
         @grid = Array.new(9) { Array.new(9) { Tile.new } }
     end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def place_bombs(count)
+        debugger
+        until count == 0 do
+            pos = [rand(9),rand(9)]
+            tile = self[pos]
+            if !tile.bombed?
+                tile.bomb
+                count -= 1
+            end
+        end
+    end
 end
+
+x = Board.new
+x.place_bombs(6)
