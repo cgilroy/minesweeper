d8dddb9218501284e891795ea87e0dc135f9bd3d
CJ Gilroy <c.gilroy9@gmail.com>
2019-11-30 08:12:15 -0700

    Add #bombed? and #bomb methods, fix attr_reader

--- Tile.rb
+++ Tile.rb
@@ -1,10 +1,18 @@
 
 class Tile
-    @attr_reader :bombed, :revealed, :flagged
+    attr_reader :bombed, :revealed, :flagged
     def initialize
         @bombed = false
         @revealed = false
         @flagged = false
+    end
+
+    def bombed?
+        @bombed
+    end
+
+    def bomb
+        @bombed = true
     end
 
 end
